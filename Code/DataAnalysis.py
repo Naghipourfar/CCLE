eaab73a671d73ef4aedf331d1a5fa7a3c4bd97a5
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-07-18 00:39:47 +0430

    Add DataAnalysis

---
+++ Code/DataAnalysis.py
@@ -0,0 +1,10 @@
+from cmapPy.pandasGEXpress.GCToo import GCToo
+from cmapPy.pandasGEXpress.parse import parse
+import numpy as np
+import pandas as pd
+
+
+DATA_PATH = "/Users/Future/Desktop/Summer 2018/Bioinformatics/CCLE/Data/CCLE_DepMap_18Q2_RNAseq_reads_20180502.gct"
+
+data = parse(DATA_PATH)
+print(data, type(data))
