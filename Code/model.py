7fc7ae90f7927502bc5daa89092aa4a37da75c11
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-08-03 10:58:21 +0430

    Generate Data

--- Code/model.py
+++ Code/model.py
@@ -1,13 +1,7 @@
-import numpy as np
-import pandas as pd
-
-import tensorflow as tf
-import matplotlib.pyplot as plt
 import keras
-
-from keras.layers import Input, Dense, Dropout, BatchNormalization
+import pandas as pd
+from keras.layers import Input, Dense
 from keras.models import Model
-from keras.callbacks import History, CSVLogger
 
 """
     Created by Mohsen Naghipourfar on 8/1/18.
@@ -17,11 +11,26 @@ from keras.callbacks import History, CSVLogger
     Skype: mn7697np
 """
 
+
 def create_model(n_features, layers):
-    input_layer = Input(shape=(n_features, ))
+    input_layer = Input(shape=(n_features,))
     dense = Dense(layers[0], activation='relu', name="dense_0")(input_layer)
     for i, layer in enumerate(layers[1:]):
         dense = Dense(layer, activation='relu', name="dense_{0}".format(i))(dense)
     model = Model(inputs=input_layer, outputs=dense)
     model.compile(optimizer=keras.optimizers.adam, loss=keras.losses.mse)
     return model
+
+
+def load_data(data_path="../Data/"):
+    data = pd.read_csv(data_path)
+
+    return None, None
+
+
+def main():
+    x_data, y_data = load_data()
+
+
+if __name__ == '__main__':
+    main()
