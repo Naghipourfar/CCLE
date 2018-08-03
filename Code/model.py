d6ee19b45dbba5c795fb89618ed0925f13f22106
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-08-03 12:26:23 +0430

    Complete model and functions

--- Code/model.py
+++ Code/model.py
@@ -2,7 +2,8 @@ import keras
 import pandas as pd
 from keras.layers import Input, Dense
 from keras.models import Model
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, normalize
+from sklearn.model_selection import train_test_split
 
 """
     Created by Mohsen Naghipourfar on 8/1/18.
@@ -42,6 +43,13 @@ def load_data(data_path="../Data/Drugs_data/drug_response.csv"):
     data['FitType'] = pd.DataFrame(keras.utils.to_categorical(label_encoder))
     y_data = data['ActArea']
     x_data = data.drop(['ActArea'], axis=1)
+    x_data, y_data = normalize_data(x_data, y_data)
+    return x_data, y_data
+
+
+def normalize_data(x_data, y_data):
+    x_data = pd.DataFrame(normalize(x_data.as_matrix(), axis=1, norm='max'))
+    y_data = pd.DataFrame(normalize(y_data.as_matrix(), axis=1, norm='max'))
     return x_data, y_data
 
 
@@ -49,6 +57,16 @@ def main():
     data_directory = '../Data/Drugs_data/'
     compound = '17-AAG.csv'
     x_data, y_data = load_data(data_path=data_directory + compound)
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.25, shuffle=True)
+
+    model = create_model(x_train.shape[1], [12500, 4096, 1024, 256, 64, 16, 4, 1])
+    model.fit(x=x_train,
+              y=y_train,
+              batch_size=128,
+              epochs=100,
+              validation_data=(x_test, y_test),
+              verbose=2,
+              shuffle=True)
 
 
 if __name__ == '__main__':
