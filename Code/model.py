99dca858119addf1a5de51881af6dd1dd0011e35
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-08-03 18:45:04 +0430

    Add Feature Selection

--- Code/model.py
+++ Code/model.py
@@ -6,6 +6,7 @@ import pandas as pd
 from keras.callbacks import CSVLogger
 from keras.layers import Input, Dense
 from keras.models import Model
+from sklearn.feature_selection import mutual_info_regression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import normalize
 
@@ -18,16 +19,16 @@ from sklearn.preprocessing import normalize
 """
 
 
-def create_model(n_features, layers, n_outputs):
+def create_model(n_features, layers, n_outputs, optimizer=None):
     input_layer = Input(shape=(n_features,))
     dense = Dense(layers[0], activation='relu', name="dense_0")(input_layer)
     for i, layer in enumerate(layers[1:]):
         dense = Dense(layer, activation='relu', name="dense_{0}".format(i + 1))(dense)
     dense = Dense(n_outputs, activation='sigmoid', name="output")(dense)
     model = Model(inputs=input_layer, outputs=dense)
-    sgd = keras.optimizers.SGD(lr=0.01, momentum=0, decay=0.9)
-    adagrad = keras.optimizers.Adagrad()
-    model.compile(optimizer=sgd, loss=["mae"], metrics=["mse", "mape"])
+    if optimizer is None:
+        optimizer = keras.optimizers.SGD(lr=0.01, momentum=0.9, decay=1e-6, nesterov=True)
+    model.compile(optimizer=optimizer, loss=["mae"], metrics=["mse", "mape"])
     return model
 
 
@@ -61,17 +62,71 @@ def normalize_data(x_data, y_data):
     return x_data, y_data
 
 
-def main():
+def feature_selection(x_data, y_data, k=500):
+    mi = mutual_info_regression(x_data, y_data)
+
+
+# def main():
+#     data_directory = '../Data/Drugs_data/'
+#     compounds = os.listdir(data_directory)
+#     optimizers = [
+#         # keras.optimizers.SGD(lr=0.1, momentum=0.9, decay=1e-6, nesterov=True),
+#         # keras.optimizers.SGD(lr=0.01, momentum=0.9, decay=1e-6, nesterov=True),
+#         # keras.optimizers.SGD(lr=0.001, momentum=0.9, decay=1e-6, nesterov=True),
+#         # keras.optimizers.Adagrad(lr=0.01, decay=1e-6)
+#         # keras.optimizers.Adadelta(lr=1.0, rho=0.95, decay=1e-6),
+#         # keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.99, decay=1e-6),
+#         keras.optimizers.Nadam(lr=0.001, beta_1=0.9, beta_2=0.999)
+#     ]
+#     print("All Compounds:")
+#     print(compounds)
+#     for compound in compounds:
+#         print("*" * 50)
+#         print(compound)
+#         print("Loading Data...")
+#         x_data, y_data = load_data(data_path=data_directory + compound)
+#         print("Data has been Loaded!")
+#         x_data, y_data = normalize_data(x_data, y_data)
+#         print("Data has been normalized!")
+#         x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3, shuffle=True)
+#         print("x_train shape\t:\t" + str(x_train.shape))
+#         print("y_train shape\t:\t" + str(y_train.shape))
+#         print("x_test shape\t:\t" + str(x_test.shape))
+#         print("y_test shape\t:\t" + str(y_test.shape))
+#         for optimizer in optimizers:
+#             model = create_model(x_train.shape[1], [1024, 64, 16], 1, optimizer)
+#             logger_path = '../Results/'
+#             if isinstance(optimizer, keras.optimizers.SGD):
+#                 session = keras.backend.get_session()
+#                 lr = session.run(optimizer.lr)
+#                 momentum = session.run(optimizer.momentum)
+#                 decay = session.run(optimizer.decay)
+#                 logger_path += '%s_SGD_lr_%1.4f_momentum_%1.4f_decay_%1.6f.log' % (
+#                     compound.split('.')[0], lr, momentum, decay)
+#             else:
+#                 logger_path += "%s_NAdam.log" % compound.split(".")[0]
+#             csv_logger = CSVLogger(logger_path)
+#             model.summary()
+#             model.fit(x=x_train,
+#                       y=y_train,
+#                       batch_size=32,
+#                       epochs=100,
+#                       validation_data=(x_test, y_test),
+#                       verbose=2,
+#                       shuffle=True,
+#                       callbacks=[csv_logger])
+#         break
+
+
+def regressor_with_k_best_features(k=50):
     data_directory = '../Data/Drugs_data/'
     compounds = os.listdir(data_directory)
-    print("All Compounds:")
-    print(compounds)
+    feature_names = list(pd.read_csv("../Data/BestFeatures.csv", header=None).loc[0, :])
     for compound in compounds:
-        print("*" * 50)
-        print(compound)
         print("Loading Data...")
         x_data, y_data = load_data(data_path=data_directory + compound)
         print("Data has been Loaded!")
+        x_data = x_data[feature_names]
         x_data, y_data = normalize_data(x_data, y_data)
         print("Data has been normalized!")
         x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3, shuffle=True)
@@ -80,19 +135,6 @@ def main():
         print("x_test shape\t:\t" + str(x_test.shape))
         print("y_test shape\t:\t" + str(y_test.shape))
 
-        model = create_model(x_train.shape[1], [1024, 256, 64, 16], 1)
-        csv_logger = CSVLogger("../Results/best_{0}.log".format(compound))
-
-        model.summary()
-        model.fit(x=x_train,
-                  y=y_train,
-                  batch_size=128,
-                  epochs=100,
-                  validation_data=(x_test, y_test),
-                  verbose=2,
-                  shuffle=True,
-                  callbacks=[csv_logger])
-
 
 if __name__ == '__main__':
-    main()
+    regressor_with_k_best_features()
