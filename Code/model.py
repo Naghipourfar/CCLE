9e2a8909d66fb04002cefcf1c1806878ba6d2861
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-08-03 12:15:17 +0430

    load data function

--- Code/model.py
+++ Code/model.py
@@ -2,6 +2,7 @@ import keras
 import pandas as pd
 from keras.layers import Input, Dense
 from keras.models import Model
+from sklearn.preprocessing import LabelEncoder
 
 """
     Created by Mohsen Naghipourfar on 8/1/18.
@@ -22,14 +23,32 @@ def create_model(n_features, layers):
     return model
 
 
-def load_data(data_path="../Data/"):
-    data = pd.read_csv(data_path)
+def load_data(data_path="../Data/Drugs_data/drug_response.csv"):
+    data = pd.read_csv(data_path, index_col='Unnamed: 0')
+    data.drop(labels=['Target',
+                      'Primary Cell Line Name',
+                      'Compound',
+                      'Doses (uM)',
+                      'Activity Data (median)',
+                      'Activity SD',
+                      'Num Data',
+                      'EC50 (uM)',
+                      'IC50 (uM)',
+                      'Amax'], axis=1, inplace=True)
 
-    return None, None
+    label_encoder = LabelEncoder()
+    label_encoder.fit(data['FitType'])
+    label_encoder = label_encoder.transform(data['FitType'])
+    data['FitType'] = pd.DataFrame(keras.utils.to_categorical(label_encoder))
+    y_data = data['ActArea']
+    x_data = data.drop(['ActArea'], axis=1)
+    return x_data, y_data
 
 
 def main():
-    x_data, y_data = load_data()
+    data_directory = '../Data/Drugs_data/'
+    compound = '17-AAG.csv'
+    x_data, y_data = load_data(data_path=data_directory + compound)
 
 
 if __name__ == '__main__':
