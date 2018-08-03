bd7b613b468c6c8987d26892b755215d0ba5d721
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-08-03 13:07:52 +0430

    Complete model

--- Code/model.py
+++ Code/model.py
@@ -1,7 +1,9 @@
 import keras
+import numpy as np
 import pandas as pd
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras.callbacks import CSVLogger, TensorBoard
 from sklearn.preprocessing import LabelEncoder, normalize
 from sklearn.model_selection import train_test_split
 
@@ -18,15 +20,16 @@ def create_model(n_features, layers):
     input_layer = Input(shape=(n_features,))
     dense = Dense(layers[0], activation='relu', name="dense_0")(input_layer)
     for i, layer in enumerate(layers[1:]):
-        dense = Dense(layer, activation='relu', name="dense_{0}".format(i))(dense)
+        dense = Dense(layer, activation='relu', name="dense_{0}".format(i+1))(dense)
     model = Model(inputs=input_layer, outputs=dense)
-    model.compile(optimizer=keras.optimizers.adam, loss=keras.losses.mse)
+    model.compile(optimizer="adam", loss=["mae"])
     return model
 
 
 def load_data(data_path="../Data/Drugs_data/drug_response.csv"):
     data = pd.read_csv(data_path, index_col='Unnamed: 0')
     data.drop(labels=['Target',
+                      'FitType',
                       'Primary Cell Line Name',
                       'Compound',
                       'Doses (uM)',
@@ -37,36 +40,48 @@ def load_data(data_path="../Data/Drugs_data/drug_response.csv"):
                       'IC50 (uM)',
                       'Amax'], axis=1, inplace=True)
 
-    label_encoder = LabelEncoder()
-    label_encoder.fit(data['FitType'])
-    label_encoder = label_encoder.transform(data['FitType'])
-    data['FitType'] = pd.DataFrame(keras.utils.to_categorical(label_encoder))
+    # label_encoder = LabelEncoder()
+    # label_encoder.fit(data['FitType'])
+    # label_encoder = label_encoder.transform(data['FitType'])
+    # data['FitType'] = pd.DataFrame(keras.utils.to_categorical(label_encoder))
     y_data = data['ActArea']
     x_data = data.drop(['ActArea'], axis=1)
-    x_data, y_data = normalize_data(x_data, y_data)
     return x_data, y_data
 
 
 def normalize_data(x_data, y_data):
-    x_data = pd.DataFrame(normalize(x_data.as_matrix(), axis=1, norm='max'))
-    y_data = pd.DataFrame(normalize(y_data.as_matrix(), axis=1, norm='max'))
+    y_data = pd.DataFrame(np.reshape(y_data.as_matrix(), (-1, 1)))
+    x_data = pd.DataFrame(normalize(x_data.as_matrix(), axis=0, norm='max'))
+    y_data = pd.DataFrame(normalize(y_data.as_matrix(), axis=0, norm='max'))
     return x_data, y_data
 
 
 def main():
     data_directory = '../Data/Drugs_data/'
     compound = '17-AAG.csv'
+    print("Loading Data...")
     x_data, y_data = load_data(data_path=data_directory + compound)
+    print("Data has been Loaded!")
+    x_data, y_data = normalize_data(x_data, y_data)
+    print("Data has been normalized!")
     x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.25, shuffle=True)
+    print("x_train shape\t:\t" + str(x_train.shape))
+    print("y_train shape\t:\t" + str(y_train.shape))
+    print("x_test shape\t:\t" + str(x_test.shape))
+    print("y_test shape\t:\t" + str(y_test.shape))
 
-    model = create_model(x_train.shape[1], [12500, 4096, 1024, 256, 64, 16, 4, 1])
+    model = create_model(x_train.shape[1], [1024, 256, 64, 16, 4, 1])
+    csv_logger = CSVLogger("../Results/best_{0}.log".format(compound))
+    # tensorboard = TensorBoard("./TensorBoard/")
+    model.summary()
     model.fit(x=x_train,
               y=y_train,
               batch_size=128,
               epochs=100,
               validation_data=(x_test, y_test),
               verbose=2,
-              shuffle=True)
+              shuffle=True,
+              callbacks=[csv_logger])
 
 
 if __name__ == '__main__':
