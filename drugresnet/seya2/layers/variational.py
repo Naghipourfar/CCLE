939d419f2fd5585bb416b4178f702238af7ce6b2
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-05-18 10:09:27 +0430

    push all remaining codes

--- drugresnet/seya2/layers/variational.py
+++ drugresnet/seya2/layers/variational.py
@@ -1,8 +1,8 @@
 from keras import backend as K
 from keras.layers.core import Layer
-from keras import initializers, activations
+from keras import initializations, activations
 
-from drugresnet.seya2.regularizers import GaussianKL
+from drugresnet.seya2 import GaussianKL
 
 
 class VariationalDense(Layer):
@@ -12,32 +12,31 @@ class VariationalDense(Layer):
         of a Gaussian distribution. During training, the output is sampled from
         that distribution as mean + random_noise * variance, during testing the
         output is the mean, i.e the expected value of the encoded distribution.
+
         Parameters:
         -----------
         batch_size: Both Keras backends need the batch_size to be defined before
             hand for sampling random numbers. Make sure your batch size is kept
             fixed during training. You can use any batch size for testing.
-        regularizer_scale: By default the regularization is already properly
+
+        regularizer_scale: By default the regularization is already proberly
             scaled if you use binary or categorical crossentropy cost functions.
             In most cases this regularizers should be kept fixed at one.
+
     """
     def __init__(self, output_dim, batch_size, init='glorot_uniform',
                  activation='tanh',
                  weights=None, input_dim=None, regularizer_scale=1,
-                 prior_mean=0, prior_logsigma=0, output_sample=False,
-                 output_var=False,
-                 **kwargs):
+                 prior_mean=0, prior_logsigma=1, **kwargs):
         self.prior_mean = prior_mean
         self.prior_logsigma = prior_logsigma
-        self.regularizer_scale = K.variable(regularizer_scale)
+        self.regularizer_scale = regularizer_scale
         self.batch_size = batch_size
-        self.init = initializers.get(init)
+        self.init = initializations.get(init)
         self.activation = activations.get(activation)
         self.output_dim = output_dim
         self.initial_weights = weights
         self.input_dim = input_dim
-        self.output_sample = output_sample
-        self.output_var = output_var
         if self.input_dim:
             kwargs['input_shape'] = (self.input_dim,)
         self.input = K.placeholder(ndim=2)
@@ -52,7 +51,7 @@ class VariationalDense(Layer):
         self.b_logsigma = K.zeros((self.output_dim,))
 
         self.trainable_weights = [self.W_mean, self.b_mean, self.W_logsigma,
-                                  self.b_logsigma]
+                       self.b_logsigma]
 
         self.regularizers = []
         reg = self.get_variational_regularization(self.get_input())
@@ -73,19 +72,14 @@ class VariationalDense(Layer):
 
     def _get_output(self, X, train=False):
         mean, logsigma = self.get_mean_logsigma(X)
-        if train or self.output_sample:
-            # Temporary change, scale down size of noise
+        if train:
             if K._BACKEND == 'theano':
-                eps = K.random_normal((X.shape[0], self.output_dim), std=self.regularizer_scale)
+                eps = K.random_normal((X.shape[0], self.output_dim))
             else:
                 eps = K.random_normal((self.batch_size, self.output_dim))
-            # Temporary change, multiply by regularizer_scale
-            return mean + self.regularizer_scale * K.exp(logsigma) * eps
+            return mean + K.exp(logsigma) * eps
         else:
-            if self.output_var:
-                return mean, logsigma
-            else:
-                return mean
+            return mean
 
     def get_output(self, train=False):
         X = self.get_input()
@@ -94,3 +88,31 @@ class VariationalDense(Layer):
     @property
     def output_shape(self):
         return (self.input_shape[0], self.output_dim)
+
+
+class TimeDistributedVAE(VariationalDense):
+    def __init__(self, time_length, **kwargs):
+        super(TimeDistributedVAE, self).__init__(**kwargs)
+        self.input = K.placeholder(ndim=3)
+        self.time_length = time_length
+        self.batch_size = self.batch_size * self.time_length
+
+    def get_variational_regularization(self, X):
+        X = K.reshape(X, (-1, self.input_shape[-1]))
+        mean = self.activation(K.dot(X, self.W_mean) + self.b_mean)
+        logsigma = self.activation(K.dot(X, self.W_logsigma) + self.b_logsigma)
+        return GaussianKL(mean, logsigma,
+                          regularizer_scale=self.regularizer_scale,
+                          prior_mean=self.prior_mean,
+                          prior_logsigma=self.prior_logsigma)
+
+    def get_output(self, train=False):
+        X = self.get_input()
+        X = K.reshape(X, (-1, self.input_shape[-1]))
+        outputs = self._get_output(X, train=train)
+        outputs = K.reshape(X, (-1, self.time_length, self.output_dim))
+        return outputs
+
+    @property
+    def output_shape(self):
+        return (self.input_shape[0], self.time_length, self.output_dim)
